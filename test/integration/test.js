0528cdf46af7845ee39e52e602f833c254e71f5c
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2019-12-12 23:32:18 +0000

    :see_no_evil: Test for 404 response

--- test/integration/test.js
+++ test/integration/test.js
@@ -7,13 +7,23 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('basic features', () => {
-  it('should return 200 response', async () => {
-    const res = await chai.request(app).get('/');
+  describe('valid route', () => {
+    it('should return 200 response as JSON', async () => {
+      const res = await chai.request(app).get('/');
 
-    expect(res).to.have.status(200);
-    // eslint-disable-next-line no-unused-expressions
-    expect(res).to.be.json;
-    expect(res.body).to.be.an('object');
-    expect(res.body).to.have.property('greeting', 'hello');
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.an('object');
+      expect(res.body).to.have.property('greeting', 'hello');
+    });
+  });
+
+  describe('unknown route', () => {
+    it('should return 404 response', async () => {
+      const res = await chai.request(app).get('/unknown');
+
+      expect(res).to.have.status(404);
+      expect(res.body).to.be.empty;
+    });
   });
 });
